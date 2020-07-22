c085b38d6c1f73b7a9cefcfdcac49a13b6fafe0d
CoffeManFP <coffemanfp@gmail.com>
2020-07-21 23:45:21 -0400

    Improving the effectiveness of validation

--- main.go
+++ main.go
@@ -16,13 +16,13 @@ var tlds string
 const allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789_-"
 
 func main() {
-	tldsArray := strings.Split(strings.Trim(tlds, " "), ",")
-
-	rand.Seed(time.Now().UTC().UnixNano())
+	tldsArray := splitStringArray(tlds)
 	s := bufio.NewScanner(os.Stdin)
+
 	for s.Scan() {
 		text := strings.ToLower(s.Text())
 		var newText []rune
+
 		for _, r := range text {
 			if unicode.IsSpace(r) {
 				r = '-'
@@ -32,11 +32,21 @@ func main() {
 			}
 			newText = append(newText, r)
 		}
+
 		fmt.Println(string(newText) + "." + tldsArray[rand.Intn(len(tldsArray))])
 	}
 }
 
+func splitStringArray(stringArray string) (array []string) {
+	array = strings.Split(strings.Replace(stringArray, " ", "", -1), ",")
+	return
+}
+
 func init() {
+	// New random generator
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	// Flags config
 	flag.StringVar(&tlds, "tlds", "com,net,org,website", "Top-level Domain names")
 
 	flag.Parse()
